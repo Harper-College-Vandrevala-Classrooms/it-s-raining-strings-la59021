708ac85999428bbe26fc1c4ea30e310c2cd97e8c
AJ Lehmann <la59021@mail.harpercollege.edu>
2024-08-26 16:03:35 -0500

    start testing

--- main.cpp
+++ main.cpp
@@ -6,35 +6,41 @@ using namespace std;
 
 vector<string> businesses;
 
-void addbusiness(string name) {
+vector<string> addbusiness(string name) {
     businesses.push_back(name);
     sort(businesses.begin(), businesses.end());
 } 
 
-void convo(){
-    bool run = true;
-    char reply;
-    string name;
-    cout << "Welcome to the Business Sorting Program!";
-    while (run) {
-        name = "";
-        reply = ' ';
-        cout << "\nWould you like to add a business? y/n\n";
-        cin >> reply;
-        if (reply != 'y') {
-            run = false;
-            break;
+void convo(bool testing){
+    if (!testing) {
+        bool run = true;
+        char reply;
+        string name;
+        cout << "Welcome to the Business Sorting Program!";
+        while (run) {
+            name = "";
+            reply = ' ';
+            cout << "\nWould you like to add a business? y/n\n";
+            cin >> reply;
+            if (reply != 'y') {
+                run = false;
+                break;
+            }
+            cout <<"\nWhat is the business's name?\n";
+            cin >> name;
+            addbusiness(name);
+            cout << "\n";
+            for (int i = 0; i < businesses.size(); i++) {
+                cout << "\n" << businesses[i];
+            }
+            cout << "\n";
         }
-        cout <<"\nWhat is the business's name?\n";
-        cin >> name;
-        addbusiness(name);
-        cout << "\n";
-        for (int i = 0; i < businesses.size(); i++) {
-            cout << "\n" << businesses[i];
-        }
-        cout << "\n";
+        cout << "\nThank you for using the Business Sorting Program!";
     }
-    cout << "\nThank you for using the Business Sorting Program!";
+    if (testing) {
+        addbusiness();
+    }
+   
 }
 
 int main() {
